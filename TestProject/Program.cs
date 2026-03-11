72f3f04d7b6a75318aa39325f842942ab182feaa
HaRin2806 <2151062809@e.tlu.edu>
2026-03-11 20:52:45 +0700

    conditional operators

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,30 +1,3 @@
-﻿/*
-Console.WriteLine("a" == "a");
-Console.WriteLine("a" == "A");
-Console.WriteLine(1 == 2);
-
-string myValue = "a";
-Console.WriteLine(myValue == "a");
-
-string value1 = " a";
-string value2 = "A ";
-Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
-
-
-Console.WriteLine("a" != "a");
-Console.WriteLine("a" != "A");
-Console.WriteLine(1 != 2);
-
-string myValue = "a";
-Console.WriteLine(myValue != "a");
-
-
-Console.WriteLine(1 > 2);
-Console.WriteLine(1 < 2);
-Console.WriteLine(1 >= 1);
-Console.WriteLine(1 <= 1);
-*/
-
-string pangram = "The quick brown fox jumps over the lay dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
+﻿Random coin = new Random();
+int flip = coin.Next(0, 2);
+Console.WriteLine(flip == 0 ? "Heads" : "Tails");
