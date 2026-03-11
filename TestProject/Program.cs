813652c2d1b11b2dfa5d15150c9ac1975b63c159
HaRin2806 <2151062809@e.tlu.edu>
2026-03-11 20:58:33 +0700

    admin test

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,3 +1,18 @@
-﻿Random coin = new Random();
-int flip = coin.Next(0, 2);
-Console.WriteLine(flip == 0 ? "Heads" : "Tails");
+﻿string permission = "Admin|Manager";
+int level = 50;
+if (permission.Contains("Admin") && level > 55)
+{
+    Console.WriteLine("Wellcome, Super Admin user.");
+}
+else if (permission.Contains("Admin") && level <= 55)
+{
+    Console.WriteLine("Welcome, Admin user");
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
