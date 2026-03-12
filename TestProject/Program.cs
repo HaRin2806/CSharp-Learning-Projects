86a5303a470faeaa547eabf47cd5fe4bdeec206c
HaRin2806 <2151062809@e.tlu.edu>
2026-03-12 21:24:15 +0700

    fizzbuzz

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,12 +1,19 @@
-﻿string[] names = { "Alex", "Eddie", "David", "Michael"};
-/*
-for (int i = names.Length - 1; i >= 0; i--)
+﻿for (int i = 1; i < 100; i++)
 {
-    Console.WriteLine(names[i]);
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} - Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
 }
-*/
-for (int i = 0; i < names.Length; i++)
-{
-    if (names[i] == "David") names[i] = "Sammy";
-}
-foreach (var name in names) Console.WriteLine(name);
