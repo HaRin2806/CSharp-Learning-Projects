dae297e12b6228b9bda857d0807e0d23b61be417
HaRin2806 <2151062809@e.tlu.edu>
2026-03-12 21:09:42 +0700

    clothes

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,21 +1,94 @@
-﻿int employeeLevel = 100;
-string employeeName = "John Smith";
-string title = "";
-switch (employeeLevel)
+﻿string sku = "01-MN-L";
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+/*
+if (product[0] == "01")
 {
-    case 100:
-    case 200:
-        title = "Senior A";
+    type = "Sweat shirt";
+}else if (product[0] == "02")
+{
+    type = "T-shirt";
+}else if (product[0] == "03")
+{
+    type = "Sweat pants";
+}else
+{
+    type = "Other";
+}
+
+if (product[1] == "BL")
+{
+    color = "Black";
+}else if (product[1] == "MN")
+{
+    color = "Maroon";
+}else
+{
+    color = "White";
+}
+
+if (product[2] == "S")
+{
+    size = "Small";
+}else if (product[2] == "M")
+{
+    size = "Medium";
+}else if (product[2] == "L")
+{
+    size = "Large";
+}else
+{
+    size = "One Size Fits All";
+}
+*/
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
         break;
-    case 300:
-        title = "Manager";
+    case "02":
+        type = "T-shirt";
         break;
-    case 400:
-        title = "Senior Manager";
+    case "03":
+        type = "Sweat pants";
         break;
     default:
-        title = "Associate";
+        type = "Other";
         break;
 }
 
-Console.WriteLine($"{employeeName}, {title}");
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Manoon";
+        break;
+    default:
+        type = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fit All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813652c2d1b11b2dfa5d15150c9ac1975b63c159")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d282ae008fbae589c3c523451fd343ba7a9ad410")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
