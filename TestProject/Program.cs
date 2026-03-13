c162798db334dd66c17c7a3ecebe9b8993c0912e
HaRin2806 <2151062809@e.tlu.edu>
2026-03-13 16:01:54 +0700

    option2

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,4 +1,5 @@
-﻿string? readResult;
+﻿/*
+string? readResult;
 int numericValue = 0;
 bool validNumber = false;
 
@@ -25,3 +26,40 @@ do
 }while (!validNumber);
 
 Console.WriteLine($"You input ({numericValue}) has been accepted.");
+*/
+//c2
+string? readResult;
+string valueEntered = "";
+int numValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null) 
+    {
+        valueEntered = readResult;
+    }
+
+    validNumber = int.TryParse(valueEntered, out numValue);
+
+    if (validNumber == true)
+    {
+        if (numValue <= 5 || numValue >= 10)
+        {
+            validNumber = false;
+            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+} while (validNumber == false);
+
+Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+
+readResult = Console.ReadLine();
+
--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224a2ba50ab2085e1797ee7667453a0bdf284cf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f9a962ea404a1c906eed1b2cbe8e1f9cae48c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
