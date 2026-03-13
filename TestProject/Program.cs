de817517956ab46f32306701fc60c70730b05ec9
HaRin2806 <2151062809@e.tlu.edu>
2026-03-13 16:05:55 +0700

    validates string input

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,65 +1,25 @@
-﻿/*
-string? readResult;
-int numericValue = 0;
-bool validNumber = false;
+﻿string? role;
+bool validRole = false;
 
-Console.WriteLine("Enter an interger value between 5 and 10");
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
 do
 {
-    readResult = Console.ReadLine();
+    role = Console.ReadLine();
 
-    if (int.TryParse(readResult, out numericValue))
+    if (role != null)
     {
-        if (numericValue >= 5 && numericValue <= 10)
+        role = role.Trim().ToLower();
+
+        if (role == "administrator" || role == "manager" || role == "user")
         {
-            validNumber = true;
+            validRole = true;
         }
         else
         {
-            Console.WriteLine($"You entered {numericValue}. Please  enter a number between 5 and 10.");
+            Console.WriteLine($"The role name that you entered, \"{role} \"is not valid. Enter your role (Administrator, Manager, User)"); 
         }
     }
-    else
-    {
-        Console.WriteLine("Sorry, you enterd an invalid number, please try again.");
-    }
-}while (!validNumber);
+}while (!validRole);
 
-Console.WriteLine($"You input ({numericValue}) has been accepted.");
-*/
-//c2
-string? readResult;
-string valueEntered = "";
-int numValue = 0;
-bool validNumber = false;
-
-Console.WriteLine("Enter an integer value between 5 and 10");
-
-do
-{
-    readResult = Console.ReadLine();
-    if (readResult != null) 
-    {
-        valueEntered = readResult;
-    }
-
-    validNumber = int.TryParse(valueEntered, out numValue);
-
-    if (validNumber == true)
-    {
-        if (numValue <= 5 || numValue >= 10)
-        {
-            validNumber = false;
-            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
-        }
-    }
-    else 
-    {
-        Console.WriteLine("Sorry, you entered an invalid number, please try again");
-    }
-} while (validNumber == false);
-
-Console.WriteLine($"Your input value ({numValue}) has been accepted.");
-
-readResult = Console.ReadLine();
-
+Console.WriteLine($"Your input value ({role}) has been accpeted.");
