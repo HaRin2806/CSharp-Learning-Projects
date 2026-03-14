c4585fb71ed5b529d84201fd85a196cc0546a426
HaRin2806 <2151062809@e.tlu.edu>
2026-03-14 15:12:09 +0700

    done_pet_manager

--- MiniProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ MiniProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de817517956ab46f32306701fc60c70730b05ec9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b3733ef8c84f25f621e1fdea68e2eb60d76614")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
